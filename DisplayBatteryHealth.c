99df10f4f688ed2f35270aece98c42fb311ac6eb
VishalSubban <VishalSubban@users.noreply.github.com>
2021-03-11 11:59:47 +0530

    Create DisplayBatteryHealth.c

---
+++ DisplayBatteryHealth.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "BMS_BatteryCheck.h"
+
+
+const char *BatteryHealthMessage[] =
+{
+    "is Unstable! Take Action Immediately!!!",
+    "is Stable"
+};
+
+
+/**
+***************************************************************************************************
+* Function Name: PrintBatteryHealthStatus
+* 
+* Function Description: Displays the BMS battery status message.
+*
+* \param  BatteryData_t BatteryData
+*         int IndexOfBatteryHealthMessage
+*         
+* \return  void
+* \retval  None
+***************************************************************************************************
+*/
+void PrintBatteryHealthStatus(BatteryData_t BatteryData, int IndexOfBatteryHealthMessage)
+{
+    printf("%s %s \n", BatteryData.BatteryParticular, BatteryHealthMessage[IndexOfBatteryHealthMessage]);
+}
